7f81f06677102c7c9da567da117b450caa0bef3e
grahampostles <graham.postles@sage.com>
2020-12-09 14:29:03 +0000

    Initial React project

---
+++ wpb-paymentsout-fileformateditor/src/components/testComponent.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import Typography from "carbon-react/lib/components/typography";
+import Button from "carbon-react/lib/components/button";
+
+const TestComponent = () => {
+    return(
+        <div>
+            <Typography variant="h1-large">Heading Level 1 Large</Typography>
+            <Button>Click me</Button>
+        </div>
+    );
+}
+
+export default TestComponent;
